e5c970e59037bdd35aed2804aab55ff1a96600cc
Michael Nguyen <mklnguyen8@gmail.com>
2020-11-30 14:47:36 -0800

    main menu variant now linked

--- Scripts/PlayButton2.gd
+++ Scripts/PlayButton2.gd
@@ -1,17 +1,4 @@
 extends Button
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-
 func _on_Button_pressed():
-	#get_tree().change_scene("")
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		get_tree().change_scene("res://Scenes/LevelTwo.tscn")
