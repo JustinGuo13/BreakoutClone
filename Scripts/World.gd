da33ac257cacb637b8c6968ada2036ee0ad9aebf
Michael Nguyen <mklnguyen8@gmail.com>
2020-12-01 01:47:30 -0800

    initialized original game to have levels

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
-var ballMaxSpeed = get_node("res://MiniScenes/Ball.tscn").maxspeed setget set_ball_max_speed
+var ballMaxSpeed = 0
 var maxspeed = 2000
 var score = 0 setget set_score
+var cumulativeScore = 0
 var life = 3 setget set_life
 var current_ball_count = 0 setget set_current_ball_count
 
+func next_Level(score):
+	if cumulativeScore == 1:
+		cumulativeScore -= 1
+		get_tree().change_scene("res://LevelOne-" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
+	
 func set_ball_max_speed(maxspeed):
 	if score >= 1000:
 		ballMaxSpeed = maxspeed*1.25
@@ -15,6 +21,8 @@ func set_ball_max_speed(maxspeed):
 func set_score(value):
 	score = value
 	get_node("Score").set_text("SCORE: " + str(score))
+	if score == 1000:
+		cumulativeScore += 1
 	
 func set_life(value):
 	life = value
